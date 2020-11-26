e06514a872a862227832835faa78de6de10ccb8b
Diego Lopez <dfelo93@gmail.com>
2020-11-25 23:35:13 -0500

    Changed && to ||

--- _strcmp.c
+++ _strcmp.c
@@ -18,7 +18,7 @@ int _strcmp(const char *s1, const char *s2)
 	int i = 0;
 	int dif;
 
-	for (i = 0 ; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		dif = s1[i] - s2[i];
 		if (dif != 0)
